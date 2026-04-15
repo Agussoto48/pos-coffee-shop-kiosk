8026b3fb525620f754b28e74bbe2a683a200822f
Juan Gonzalez <JUAN.GONZALEZMARQUEZ@ucr.ac.cr>
2026-04-15 09:33:12 -0600

    feat(cart): add ClearCart use case

---
+++ src/pos_coffee_shop_kiosk/application/use_cases/shopping_cart/clear_cart.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+class ClearCart:
+    def __init__(
+        self,
+        shopping_cart: ShoppingCart,
+    ) -> None:
+        self.shopping_cart = shopping_cart
+
+    def execute(self) -> None:
+        pass
